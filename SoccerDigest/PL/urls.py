d6e06fa836aaa4643a7caaeb23ed6b3db8e0dfa6
Sshetty-1234 <sohamshetty100@gmail.com>
2024-12-16 23:44:33 -0500

    testing

--- SoccerDigest/PL/urls.py
+++ SoccerDigest/PL/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.soccer, name='soccer'),
     path('squad/<str:name>', views.team_info, name = "team"),
-    path('standing/',views.rankings)
+    path('standing/',views.rankings),
+    path('index/',views.blog_index)
     
 ]
